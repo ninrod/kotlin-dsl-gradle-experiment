d31ae65f60d67f6c9fb45422f79f03378f66a364
Filipe Correa Lima da Silva <filipe.silva@bcb.gov.br>
2019-01-22 16:57:03 -0200

    working

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,14 +10,14 @@ val postgresql_driver_version: String by project
 val springboot_version: String by project
 
 buildscript {
-    val artifactory = "http://artifactory/artifactory/gradle"
     repositories {
         // BUG HERE: I have to use the full qualified name of the doWeHaveToUseArtifactory
         // because the buildScript block does not respect the top level defined imports!
         if (org.ninrod.backend.build.doWeHaveToUseArtifactory()) {
             println("configuring artifactory for plugin repos")
             maven {
-                url = uri(artifactory)
+                val artifactory_gradle: String by project
+                url = uri(artifactory_gradle)
             }
         } else {
             println("we are using mavencentral for plugins")
